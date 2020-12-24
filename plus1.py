dbae04233df93846a59f071408ddf4b5ebffc020
kietdiep <kietddiep@gmail.com>
2020-12-23 23:02:42 -0800

    2nd attempt with reversed iterations work better

--- plus1.py
+++ plus1.py
@@ -1,27 +1,34 @@
 import math
 
-digits = [4,3,2,1]
+digits = [9,9,9,9,9]
 
 
 def plusOne(digits):
+    if len(digits) == 1 and digits[0] == 9:
+        return [1,0]
     counter = len(digits)-1
     sum = 0
     for num in digits:
         sum = sum + num * 10 ** counter
+        print(f'added sum is {sum}')
         if counter > 0:
             counter -= 1
     sum += 1
-    counter = len(digits)-1
+    print(f'sum + 1: {sum}')
+    counter = len(str(sum))-1
+    print(f'counter: {counter}')
     array = []
     while counter >= 0:
         temp = math.floor(sum / 10**counter)
+        print(f'temp: {temp}')
         array.append(temp)
         sum = sum % (temp * 10 ** counter)
+        print(sum)
         counter -= 1
     return array    
 
 
-print(plusOne(digits))
+#print(plusOne(digits))
 
 
 #  4 3 2 1
@@ -30,4 +37,19 @@ print(plusOne(digits))
 #  2 * 10 ** 1 =  20
 #  1 * 10 ** 0 =   1
 
+# Attempt 1 ----------------------------------------------------------
 
+def plus1(digits):
+    dig_len = len(digits)
+    for i in reversed(range(dig_len)):
+        digits[i] += 1
+        if digits[i] < 10:
+            return digits
+        else:
+            digits[i] = 0
+    if digits[0] == 0:
+        digits.insert(0,1)
+    return digits
+
+
+print(plus1(digits))
