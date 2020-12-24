d2064dc392f9bf007b6b022cfcb1e4a8fd8ba1aa
kietdiep <kietddiep@gmail.com>
2020-12-23 20:16:22 -0800

    final attempt, realized overthinking the question.

--- intersect.py
+++ intersect.py
@@ -18,4 +18,23 @@ def intersect(nums1, nums2):
 print(intersect(nums1,nums2))
 # initial attempt somewhat works but fails to work when the smaller array has multiple intersections
 
+def intersect2(nums1,nums2):
+    i = 0
+    j = 0
+    result = []
+    nums1.sort()
+    nums2.sort()
 
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            result.append(nums1[i])
+            i += 1
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        else:
+            j += 1
+    return result
+
+
+print(intersect2(nums1,nums2))
