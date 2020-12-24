a238c677d4786cdb8f10f7149047381ff4503d3d
kietdiep <kietddiep@gmail.com>
2020-12-23 18:30:31 -0800

    final version with added library assistance

--- singleNum.py
+++ singleNum.py
@@ -28,3 +28,13 @@ print(uniqueNum([1,2,2,1,3]))
 # Better solution with linear runtime complexity ----------------------------------------
 # bitwise XOR operator solves it
 # anything times itself is 0 but if it only occurs once it will come out as 1
+
+import operator
+from functools import reduce
+
+def unique_element(nums):
+    return reduce(operator.xor, nums)
+
+print(unique_element([1,2,2,1,3]))
+
+# using library this time to do same thing
